994c5bf5209a412b1809d553ed150d0fee31e382
Nikhitha <nbk217@gmail.com>
2020-12-13 20:44:59 -0600

    Find Project by Identifier

--- src/main/java/com/myproject/ppmtool/services/ProjectService.java
+++ src/main/java/com/myproject/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.myproject.ppmtool.services;
 
 import com.myproject.ppmtool.domain.Project;
+import com.myproject.ppmtool.exceptions.ProjectIdException;
 import com.myproject.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,13 +12,27 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-//    public void findAllProjects(Long id){
-//
-//       return projectRepository.findAllById(id);
-//    }
+    public Project findProject(String id){
+       return projectRepository.findById(Long.parseLong(id)).get();
+    }
 
+    public Project findProjectByIdentitifer(String projectId){
+
+        Project project= projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project==null){
+            throw new ProjectIdException("Project ID '"+ projectId.toUpperCase()+"' doesn't exist");
+
+        }
+        return project;
+    }
     public Project saveOrUpdateProject(Project project){
 
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch(Exception e){
+            throw new ProjectIdException("Project ID '"+ project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
     }
 }
--- src/main/java/com/myproject/ppmtool/web/ProjectController.java
+++ src/main/java/com/myproject/ppmtool/web/ProjectController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -31,7 +28,6 @@ public class ProjectController {
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-
         ResponseEntity<?> errorMap= mapValidationErrorService.mapValidationService(result);
         if(errorMap!=null){
             return  errorMap;
@@ -39,5 +35,12 @@ public class ProjectController {
 
         Project project1= projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId){
+       Project project= projectService.findProjectByIdentitifer(projectId);
+        return new ResponseEntity<Project>(project,HttpStatus.OK);
+
     }
 }
